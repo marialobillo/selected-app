8fda2f8199a693ff6eb9fca0ce68c855b9bd39d8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-08 19:08:07 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import rootReducer from './reducers';
+
 ReactDOM.render(<App/>, document.getElementById('root'));
