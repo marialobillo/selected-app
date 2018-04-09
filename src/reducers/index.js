6e7aeb79ccb73ed6019d2e7c7955087f44a0855d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-09 10:52:40 +0200

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,37 @@
 import characters_json from '../data/characters.json';
+import { ADD_CHARACTER } from '../actions';
+import { combineReducers } from 'redux';
 
 function characters(state = characters_json, action){
 	switch (action.type) {
+
+		case ADD_CHARACTER:
+			let characters = state.filter(item => item.id !== action.id);
+			return characters;
 		default:
 			return state;
 
 	}
 }
 
-export default characters;
+function heroes(state = [], action){
+	switch (action.type) {
+		case ADD_CHARACTER:
+			let heroes = [...state, createCharacter(action.id)];
+			return heroes;
+		default:
+			return state;
+	}
+}
+
+function createCharacter(id){
+	let character = characters_json.find(c => c.id === id);
+	return character;
+}
+
+const rootReducer = combineReducers({
+	characters,
+	heroes
+});
+
+export default rootReducer;
