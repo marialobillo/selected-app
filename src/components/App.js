98e035a0b6aa31674eefb2db57bac743d654e13b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-09 17:43:23 +0200

    character list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import CharacterList from './CharacterList';
 
 class App extends Component{
 	render(){
 		return (
 			<div>
 				<h2>Super Squad</h2>
+				<CharacterList/>
 			</div>
 		);
 	}
