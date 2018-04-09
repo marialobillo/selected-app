d8269c6a05efb02ddcfeed5e8a09b6acb563aef7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-09 11:07:53 +0200

    refactoring : heroes, characters, helpers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,8 @@
-import characters_json from '../data/characters.json';
-import { ADD_CHARACTER } from '../actions';
 import { combineReducers } from 'redux';
+import characters from './character_reducer';
+import heroes from './heroes_reducer';
 
-function characters(state = characters_json, action){
-	switch (action.type) {
 
-		case ADD_CHARACTER:
-			let characters = state.filter(item => item.id !== action.id);
-			return characters;
-		default:
-			return state;
-
-	}
-}
-
-function heroes(state = [], action){
-	switch (action.type) {
-		case ADD_CHARACTER:
-			let heroes = [...state, createCharacter(action.id)];
-			return heroes;
-		default:
-			return state;
-	}
-}
-
-function createCharacter(id){
-	let character = characters_json.find(c => c.id === id);
-	return character;
-}
 
 const rootReducer = combineReducers({
 	characters,
