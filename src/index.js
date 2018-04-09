1f0e2fa3c676437655008dcb609e3ca838b98d8b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-09 09:38:50 +0200

    adding an action

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import rootReducer from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
+	,
+	document.getElementById('root'));
