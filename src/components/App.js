61bafce6b549b06a37432b453c5ea63bf350e025
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-09 19:13:25 +0200

    adding hero list

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import CharacterList from './CharacterList';
+import HeroList from './HeroList';
 import '../styles/index.css';
 
 class App extends Component{
 	render(){
 		return (
 			<div className="App">
-				<h2>Super Squad</h2>
-				<CharacterList/>
+				<div className="row">
+					<h2>Super Squad</h2>
+					<div className="col-md-6">
+						<CharacterList/>
+					</div>
+					<div className="col-md-6">
+						<HeroList />
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
+
+
 
 export default App;
---
+++ src/components/HeroList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class HeroList extends Component{
+
+	render(){
+		return(
+			<div>
+				<h4>Your Hero Squad</h4>
+				<ul>
+					{
+						this.props.heroes.map(hero => {
+							return (
+								<li key={hero.id} className="list-group-item">
+									<div className="list-item">
+										{hero.name}
+									</div>
+								</li>
+							);
+						})
+					}
+				</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state){
+	return {
+		heroes: state.heroes
+	}
+}
+
+export default connect(mapStateToProps, null)(HeroList);
